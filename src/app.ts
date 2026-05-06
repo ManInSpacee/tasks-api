0f5347662b0c170579039cd73f9d5174772c7cc0
ManInSpace <klimentmart@gmail.com>
2026-05-07 00:02:10 +0300

    chore: update launch scripts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import tasksRouter from "./routes/tasks.route";
 
 const app = express();
-
 app.use(express.json());
+
+app.use("/tasks", tasksRouter);
 
 export default app;
